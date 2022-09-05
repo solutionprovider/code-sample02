1a06d9f93743050ea2907d43ee63ecfebb4a1f10
Muneer <test@test.com>
2022-09-05 22:01:02 +0500

    Update Program.cs

--- jenkins-demo/HRM/HRM.API/Program.cs
+++ jenkins-demo/HRM/HRM.API/Program.cs
@@ -20,18 +20,7 @@ builder.Services.AddSwaggerGen(options =>
     {
         Version = "v1",
         Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
-        }
+        Description = "An ASP.NET Core Web API for managing ToDo items"
     });
 });
 
