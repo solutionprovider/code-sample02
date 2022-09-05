56e4885ff2c04d5b8f5522766a2d3f23eb6a37ae
Muneer <test@test.com>
2022-09-05 21:56:49 +0500

    Update Program.cs

--- jenkins-demo/HRM/HRM.API/Program.cs
+++ jenkins-demo/HRM/HRM.API/Program.cs
@@ -13,12 +13,33 @@ builder.Services.AddDbContext<HRMContext>(opt => opt.UseInMemoryDatabase("HRMDB"
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
 
 
 //Register DI
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+
 
 
 var app = builder.Build();
